c0a0cc172a45803de0aa42724cf7761b504706ad
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-19 00:48:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,8 +13,7 @@ return result
 end
 
 
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
+
 def gross_for_director(director_data)
   total = 0
   gross_index = 0
