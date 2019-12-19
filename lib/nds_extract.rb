a74d847156e4357262350685dd23334b8f5b493c
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-12-19 00:43:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ require 'pp'
 def directors_totals(nds)
   result = {}
   name_index = 0
-  while name_index < nds
+  while name_index < nds.length
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
